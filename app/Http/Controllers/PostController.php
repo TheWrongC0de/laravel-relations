1900c3cc3bc1c093f0e6e027d0a83c1932efddb2
Simone <simoneugazio1993@gmail.com>
2020-01-27 19:03:12 +0100

    Fine

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Tag;
+use App\Post;
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::paginate();
+
+        return view('index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {   
+        $categories = Category::all();
+        return view('pages.postStore', compact("categories"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {   
+        
+        $validatedData = $request -> validated();
+        $post = Post::create($validatedData);
+        return redirect() -> route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        $categories = Category::all();
+        $post = Post::findOrFail($post -> id);
+        return view('pages.postShow', compact("post" ,"categories"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $data = [
+            'categories' => Category::all(),
+            'post' => $post
+        ];
+
+        return view('edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $data = $request->all();
+
+        //aggiorno il post con tutti i data (lui prenderà solo quelli che interessano alla tabella posts, attraverso la variabile fillable che abbiamo messo nel model)
+        $post->update($data);
+
+        //Stessa cosa per i postinformation
+        $post->postInformation->update($data);
+
+        //ritorniamo alla home
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //Prima cancello la tabella associata, altrimenti non potrei cancellare post che è la tabella padre
+        $post->postInformation->delete();
+        $post -> tags() -> sync([]);
+        $post = Post::findOrFail($post -> id);
+
+        $post->delete();
+
+        return redirect()-> route('posts.index');
+    }
+}
